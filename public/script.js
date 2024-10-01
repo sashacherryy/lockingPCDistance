e84aa7e02404d8c0e2f0771c434d7a398925db63
Olexandr Vyshnewscky <100805336+sashacherryy@users.noreply.github.com>
2024-10-01 12:12:29 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const statusDiv = document.getElementById('status');
     const lockButton = document.getElementById('lockButton');
 
-    const serverUrl = 'http://192.168.x.x:3001/lock'; // local ipv4 + server port + /lock *local ipv4 you can find in Win + r -> CMD -> ipconfing
+    const serverUrl = 'http://192.168.x.x:3001/lock'; // local ipv4 + server port + /lock *local ipv4 you can find in Win + r -> CMD -> ipconfig
 
     lockButton.addEventListener('click', function() {
         fetch(serverUrl, { method: 'POST' })
